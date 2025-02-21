96c5aa8e9e48b84e1187a6714eb4232f377a725e
rozziequ <54429754+rozziequ@users.noreply.github.com>
2025-02-21 13:20:18 +0300

    Update Lab 9 Task 19.py

--- Lab 9 Task 19.py
+++ Lab 9 Task 19.py
@@ -1,6 +1,6 @@
-# Сформировать массив из положительных элементов заданного массива размером 10.
+# Если максимальный элемент массива больше суммы элементов массива, заменить его нулем, иначе - удвоить.
 
-file = open('D:/OneDrive/Документы/Lab 9/Lab 9.txt', 'r')
+file = open('D:\OneDrive\Документы\Lab 9\Lab 9 Text.txt', 'r')
 s = file.read()   # Считывание текста
 print('Изначальный текст:', s)
 s_split = s.split()
@@ -29,3 +29,15 @@ else:
             s_split[i] = 2 * s_split[i]
 
 print('Конечный результат:', s_split)
+
+file.close()
+
+writing = ''
+for i in range(9):
+    writing += str(s_split[i])
+    writing += ' '
+writing += str(s_split[9])
+
+file = open('D:\OneDrive\Документы\Lab 9\Lab 9 Writing.txt', 'w')
+file.write(writing)
+file.close()
